f39cd2b24c707e780ac93bc7ebafc0040d96e1bb
Robert Czechowski <robert+git@czechowski.de>
2024-12-30 23:52:34 +0100

    Make chapter 3 code work on MacOS

--- chapter3_triangle/src/main.rs
+++ chapter3_triangle/src/main.rs
@@ -35,9 +35,10 @@ void main() {
 ";
 
 const FRAGMENT_SHADER_SOURCE: &CStr = c"#version 410 core
+out vec4 color;
 
 void main() {
-    gl_FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
 ";
 
